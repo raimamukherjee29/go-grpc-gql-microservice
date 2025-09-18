ca31d7f7612ecb1dd9175bc3665e0525d5a27b94
unknown <raimamukherjee2910@gmail.com>
2025-09-19 05:16:36 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/raimamukherjee29/go-grpc-gql-microservice
+
+go 1.25.1
+
+require (
+	github.com/99designs/gqlgen v0.17.80
+	github.com/vektah/gqlparser/v2 v2.5.30
+	google.golang.org/grpc v1.75.1
+	google.golang.org/protobuf v1.36.9
+)
+
+require (
+	github.com/agnivade/levenshtein v1.2.1 // indirect
+	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
+	github.com/tinrab/retry v1.0.0 // indirect
+	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
+)
---
+++ graphql/graph.go
@@ -0,0 +1,50 @@
+package main
+
+type Server struct {
+	accountClient *account.Client
+	catalogClient *catalog.Client
+	orderClient   *order.Client
+}
+
+func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+	accountClient, err := account.NewClient(accountUrl)
+
+	if err != nil {
+		return nil, err
+	}
+
+	catalogClient, err := catalog.NewClient(catalogUrl)
+	if err != nil {
+		accountClient
+		return nil, err
+	}
+
+	orderClient, err := order.NewClient(orderUrl)
+	if err != nil {
+		accountClient.Close()
+		catalogClient.Close()
+		return nil, err
+	}
+
+	return &Server{
+		accountClient: accountClient,
+		catalogClient: catalogClient,
+		orderClient:   orderClient,
+	}, nil
+}
+
+// func (s *Server) Mutation() MutationResolver {
+// 	return &mutationResolver{server: s}
+// }
+
+// func (s *Server) Query() QueryResolver {
+// 	return &queryResolver{server: s}
+// }
+
+// func (s *Server) Account() AccountResolver {
+// 	return &accountResolver{server: s}
+// }
+
+// func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
+// 	return NewExecutableSchema(Config{Resolvers: s})
+// }
